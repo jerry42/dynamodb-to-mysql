7fff746bc95739ff6e5c0ce04a3f8b084c9bdf07
Jeremy Sculfort <jeremy.sculfort@gmail.com>
2024-04-06 17:43:19 +0200

    update default concurrency

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ process.env.MYSQL_HOST = '127.0.0.1'
 process.env.MYSQL_USER = 'root'
 process.env.MYSQL_PASSWORD = 'password'
 process.env.MYSQL_DATABASE = 'my_database'
-process.env.CONCURRENCY = 1000
+process.env.CONCURRENCY = 100
 
 // You need to be authenticated to AWS CLI
 process.env.AWS_ACCESS_KEY_ID='AKIAIOSFODNN7EXAMPLE'
